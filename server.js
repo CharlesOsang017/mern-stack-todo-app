1789cc82ba9288f5c91c1b05f608c28a14a8d2b0
CharlesOsang017 <charlesosango02@gmail.com>
2022-04-18 12:15:19 +0300

    Initial set up

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  res.send('React Node js course');
+});
+
+app.post("/name", (req, res) => {
+    if (req.body.name) {
+        return res.json({name: req.body.name})
+    } else {
+        return res.status(400).json({error: 'No name provide'})
+    }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
